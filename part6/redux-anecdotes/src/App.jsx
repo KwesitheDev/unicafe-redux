0a9e405344d690755a2e85265197e6d8163522c5
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-17 05:09:46 +0100

    added votehandler

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,11 +1,13 @@
 import { useSelector, useDispatch } from 'react-redux'
+import {voteAnecdote} from './reducers/anecdoteReducer'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
 
-  const vote = (id) => {
+  const handleVote = (id) => {
     console.log('vote', id)
+    dispatch(voteAnecdote(id))
   }
 
   return (
@@ -18,7 +20,7 @@ const App = () => {
           </div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
+            <button onClick={() => handleVote(anecdote.id)}>vote</button>
           </div>
         </div>
       )}
