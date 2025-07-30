1b8b34497031eb7d820063b953a03de10d06d048
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-30 01:20:10 +0000

    added FilterList

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,11 @@
 import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList  from './components/AnecdoteList'
-
+import AnecdoteList from './components/AnecdoteList'
+import FilterList from './components/AnecdoteFilter'
 
 const App = () => {
   return (
     <div>
+      <FilterList />
       <AnecdoteList />
       <AnecdoteForm />
     </div>
