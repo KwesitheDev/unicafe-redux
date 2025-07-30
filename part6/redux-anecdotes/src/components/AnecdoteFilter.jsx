b3063ca978cebb86ef30d276f00af2f21bf73143
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-30 01:05:31 +0000

    filterList

---
+++ part6/redux-anecdotes/src/components/AnecdoteFilter.jsx
@@ -0,0 +1,22 @@
+import { useSelector, useDispatch } from "react-redux";
+import { filterChange } from "../reducers/filterReducer";
+
+const filterList = () => {
+    const dispatch = useDispatch()
+    const filter = useSelector(state = state.filter)
+
+    const handleAnecdoteChange = (e) => {
+        dispatch(filterChange(e.target.value))
+    }
+
+    return (
+        <div>
+            filter <input
+                onChange={handleAnecdoteChange}
+                placeholder="filter anecdotes" />
+
+        </div>
+    )
+}
+
+export default filterList
