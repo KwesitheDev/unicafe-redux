cb1f0caeacda68473a8cb0cf3bec7c6496885a69
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-03 05:13:47 +0000

    Notification component added

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,12 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import FilterList from './components/AnecdoteFilter'
+import Notification from './components/Notification'
 
 const App = () => {
   return (
     <div>
+      <Notification />
       <FilterList />
       <AnecdoteList />
       <AnecdoteForm />
