9353958bee8f4d61b703cfc47c28483f3cfd2f2a
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-03 05:18:09 +0000

    imported clear interval, and set notification to 5 secs

--- part6/redux-anecdotes/src/components/Notification.jsx
+++ part6/redux-anecdotes/src/components/Notification.jsx
@@ -1,4 +1,8 @@
 import { useSelector } from "react-redux";
+import { clearNotification } from "../reducers/notificationReducer";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 const Notification = () => {
   const notification = useSelector(state => state.notification)
   const style = {
@@ -6,6 +10,18 @@ const Notification = () => {
     padding: 10,
     borderWidth: 1
   }
+  const dispatch = useDispatch()
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        dispatch(clearNotification())
+      }, 5000)
+      return () => clearTimeout(timer)
+    }
+  }, [notification, dispatch])
+
+  if (!notification) return null
+
   return (
     <div style={style}>
       {notification}
