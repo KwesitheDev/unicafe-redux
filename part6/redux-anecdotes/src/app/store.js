4953a2215702e8a6598887676d0be5bd58661871
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-03 12:04:11 +0000

    ex 6.14 completed -fetches data from backend

--- part6/redux-anecdotes/src/app/store.js
+++ part6/redux-anecdotes/src/app/store.js
@@ -1,7 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import anecdoteReducer from '../reducers/anecdoteReducer'
+import anecdoteReducer, { setAnecdotes } from '../reducers/anecdoteReducer'
 import filterReducer from '../reducers/filterReducer'
 import notificationReducer from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 export const store = configureStore({
     reducer: {
         anecdotes: anecdoteReducer,
@@ -9,4 +10,11 @@ export const store = configureStore({
         notification: notificationReducer
     }
 })
+
+export const fetchAnecdote = () => {
+    anecdoteService.getAll().then(anecdotes => {
+        store.dispatch(setAnecdotes(anecdotes))
+    })
+}
+
 
--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -2,9 +2,9 @@ import ReactDOM from 'react-dom/client'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import App from './App'
-import { store } from './app/store'
+import { store, fetchAnecdote } from './app/store'
 
-
+fetchAnecdote()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
