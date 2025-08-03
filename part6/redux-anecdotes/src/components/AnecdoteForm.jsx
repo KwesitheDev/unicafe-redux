6380359bba69bde9c8aac324f4dbb441f53857bf
kwesithedev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-03 04:55:34 +0000

    dispatched setCreateNotification in handleSubmit

--- part6/redux-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,24 +1,27 @@
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
+import { setCreateNotification } from '../reducers/notificationReducer'
 
 
 
-  
+
+
 const AnecdoteForm = () => {
-    const dispatch = useDispatch()
-    const handleCreate = (event) => {
+  const dispatch = useDispatch()
+  const handleCreate = (event) => {
     event.preventDefault()
     const content = event.target[0].value
     dispatch(createAnecdote(content))
+    dispatch(setCreateNotification(content))
     event.target[0].value = ''
-}
+  }
   return (
     <div>
-          <h2>create new</h2>
-          <form onSubmit={handleCreate}>
-            <div> <input /></div>
-             <button type="submit">create</button>
-          </form>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div> <input /></div>
+        <button type="submit">create</button>
+      </form>
     </div>
   )
 }
