4eaba2f8dfaf9239c6b7a154c2fd2ebabdae91a9
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-19 16:38:41 +0100

    createAnecdote

--- part6/query-anecdotes/src/services/requests.js
+++ part6/query-anecdotes/src/services/requests.js
@@ -1,5 +1,11 @@
 import axios from 'axios'
+const baseUrl = 'http://localhost:3001/anecdotes'
+export const getAnecdotes = async () => {
+    const res = await axios.get(baseUrl)
+    return res.data
+}
 
-export const getAnecdote = () => {
-    return axios.get('http://localhost:3001/anecdotes')
+export const createAnecdote = async (content) => {
+    const res = await axios.post(baseUrl, content)
+    return res.data
 }
