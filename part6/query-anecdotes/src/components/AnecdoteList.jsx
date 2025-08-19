ab7dffc26452013c93d4ed675aa0f3eee1c08b69
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-19 20:02:50 +0100

    AnecdoteList component

---
+++ part6/query-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,43 @@
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, updateAnecdote } from '../services/requests'
+
+const AnecdoteList = () => {
+  const queryClient = useQueryClient()
+
+  const { data: anecdotes, isLoading } = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+  })
+
+  const voteMutation = useMutation({
+    mutationFn: updateAnecdote,
+    onSuccess: (updatedAnecdote) => {
+      // Update cache instead of refetching whole list
+      queryClient.setQueryData(['anecdotes'], (old) =>
+        old.map((a) => (a.id === updatedAnecdote.id ? updatedAnecdote : a))
+      )
+    },
+  })
+
+  if (isLoading) return <div>loading anecdotes...</div>
+
+  const handleVote = (anecdote) => {
+    voteMutation.mutate({ ...anecdote, votes: anecdote.votes + 1 })
+  }
+
+  return (
+    <div>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AnecdoteList
